b83dcb1f86a50d6cf51c8e8537af8562488790fd
30knees <30377771+30knees@users.noreply.github.com>
2025-05-05 15:48:05 +0200

    Update cron.php

--- cron.php
+++ cron.php
@@ -23,22 +23,22 @@
  */
 
 include_once('../../config/config.inc.php');
-include_once('okom_vip.php');
+include_once('memberprime.php');
 echo 'Start clean old VIP Card<br/><br/>';
-if (Tools::getValue('token') != Tools::encrypt('okom_vip') || !Module::isInstalled('okom_vip')) {
+if (Tools::getValue('token') != Tools::encrypt('memberprime') || !Module::isInstalled('memberprime')) {
     echo 'OUPS !';
     die();
 } else {
-    $module = new okom_vip();
+    $module = new memberprime();
     $sql = 'SELECT * FROM '._DB_PREFIX_.'vip'.' WHERE NOW() >= vip_end AND expired = 0';
     $old_vip_cards = Db::getInstance()->ExecuteS($sql);
 
     foreach ($old_vip_cards as $old_vip_card) {
-        Db::getInstance()->delete('customer_group', 'id_customer = '.(int)$old_vip_card['id_customer'].' AND id_group = '.(int)Configuration::get('OKOM_VIP_IDGROUP'));
+        Db::getInstance()->delete('customer_group', 'id_customer = '.(int)$old_vip_card['id_customer'].' AND id_group = '.(int)Configuration::get('memberprime_IDGROUP'));
         $module->setExpired((int)$old_vip_card['id_vip']);
     }
 
     echo date('Y-m-d H:i:00').'<br/>';
-    Configuration::updateValue('OKOM_VIP_CLEAN', date('Y-m-d H:i:00'));
+    Configuration::updateValue('memberprime_CLEAN', date('Y-m-d H:i:00'));
     echo 'Done';
 }
